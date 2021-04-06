8a0c4ff0dcbc146c419b78aa357e4e76cf437c90
MhdAvicennaBadri <80657129+MhdAvicennaBadri@users.noreply.github.com>
2021-04-06 21:52:48 +0700

    More On Classes

---
+++ MoreOnClasses/ComparingObjects.java
@@ -0,0 +1,13 @@
+package MoreOnClasses;
+
+public class ComparingObjects {
+    private int x;
+    public static void main(String[] args) {
+        ComparingObjects a = new ComparingObjects();
+        a.x = 5;
+        ComparingObjects b = new ComparingObjects();
+        b.x = 5;
+        System.out.println(a == b);
+    }
+}
+
