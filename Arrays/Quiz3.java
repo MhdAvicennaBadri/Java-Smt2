85dfbdbe5f3f568db3ba1a037f563d545c23340e
MhdAvicennaBadri <80657129+MhdAvicennaBadri@users.noreply.github.com>
2021-04-06 17:26:02 +0700

    Arrays

---
+++ Arrays/Quiz3.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class Quiz3 {
+    public static void main(String[] args) {
+        int arr[] = new int [3];
+        int res = 0;
+        for (int el: arr){
+            res += el;
+        }
+
+    }
+}
